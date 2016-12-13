9d914f1375a6117a06997de26c76f630a76a1cd6
Michael Maguire <github@michaelmaguire.ca>
2016-12-13 13:58:39 +0000

    Now command line help working

--- src/jepsen/zookeeper.clj
+++ src/jepsen/zookeeper.clj
@@ -2,7 +2,7 @@
   (:gen-class) (:require [jepsen.cli :as cli] [jepsen.tests :as tests])
   ) 
 
-(defn zk-test "Given an options map from the command-line runner (e.g. :nodes, :ssh, :concurrency, ...), constructs a test map." [opts] tests/noop-test)
+(defn zk-test "Given an options map from the command-line runner (e.g. :nodes, :ssh, :concurrency, ...), constructs a test map." [opts] (merge tests/noop-test {:nodes (:nodes opts) :ssh (:ssh opts)}))
 
 (defn -main
   "Handle command line arguments"
