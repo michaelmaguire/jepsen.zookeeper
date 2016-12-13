d3fb5bd880f99c46499d3dccc1dc509b26653848
Michael Maguire <github@michaelmaguire.ca>
2016-12-13 14:21:20 +0000

    14:18:12.625 [main] INFO  jepsen.cli - Test options:  {:concurrency 1,  :test-count 1,  :time-limit 60,  :nodes-file "../nodes",  :nodes ["w1"],  :ssh  {:username "admin",   :password "root",   :strict-host-key-checking false,   :private-key-path nil}}
    INFO [2016-12-13 14:18:13,083] jepsen nemesis - jepsen.core Nemesis starting
    INFO [2016-12-13 14:18:13,083] jepsen nemesis - jepsen.core nemesis done
    INFO [2016-12-13 14:18:13,084] jepsen worker 0 - jepsen.core Worker 0 starting
    INFO [2016-12-13 14:18:13,085] jepsen worker 0 - jepsen.core Worker 0 done
    INFO [2016-12-13 14:18:13,085] jepsen test runner - jepsen.core Waiting for nemesis to complete
    INFO [2016-12-13 14:18:13,085] jepsen test runner - jepsen.core nemesis done.
    INFO [2016-12-13 14:18:13,085] jepsen test runner - jepsen.core Tearing down nemesis
    INFO [2016-12-13 14:18:13,085] jepsen test runner - jepsen.core Nemesis torn down
    INFO [2016-12-13 14:18:13,086] jepsen test runner - jepsen.core Run complete, writing
    INFO [2016-12-13 14:18:13,108] jepsen test runner - jepsen.core Analyzing
    INFO [2016-12-13 14:18:13,119] jepsen test runner - jepsen.core Analysis complete
    INFO [2016-12-13 14:18:13,132] jepsen results - jepsen.store Wrote /home/admin/jepsen.zookeeper/store/noop/20161213T141812.000Z/results.edn
    INFO [2016-12-13 14:18:13,139] main - jepsen.core {:valid? true,
     :configs ({:model {}, :last-op nil, :pending []}),
     :final-paths ()}
    
    Everything looks good! ヽ(‘ー`)ノ

--- src/jepsen/zookeeper.clj
+++ src/jepsen/zookeeper.clj
@@ -1,8 +1,30 @@
 (ns jepsen.zookeeper
-  (:gen-class) (:require [jepsen.cli :as cli] [jepsen.tests :as tests])
-  ) 
+  (:gen-class) 
+  (:require [jepsen [cli :as cli] 
+             [db :as db]
+             [control :as c]
+             [tests :as tests]]
+            [jepsen.os.debian :as debian]
+            [clojure.tools.logging :refer :all]
+            )
+  )
 
-(defn zk-test "Given an options map from the command-line runner (e.g. :nodes, :ssh, :concurrency, ...), constructs a test map." [opts] (merge tests/noop-test {:nodes (:nodes opts) :ssh (:ssh opts)}))
+(defn db "A zookeeper DB at the given version"
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (info "Setting up ZK on " node))
+    (teardown! [_ test node]
+      (info node "tearing down ZK"))))
+
+
+(defn zk-test "Given an options map from the command-line runner (e.g. :nodes, :ssh, :concurrency, ...), constructs a test map." 
+  [opts] 
+  (merge tests/noop-test 
+         {
+          :nodes (:nodes opts) 
+          :ssh (:ssh opts)
+          }))
 
 (defn -main
   "Handle command line arguments"
